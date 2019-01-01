f97f94201ca8fc78ad6ffd3d8b0961bccb78d2eb
John Otugo <jtotugo@ymail.com>
2019-01-01 22:20:02 +0000

    I added a new welcome script

--- Tanker/src/Main.java
+++ Tanker/src/Main.java
@@ -1,13 +1,16 @@
-
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Dumebs is a special girl.");
+      //  System.out.println("Dumebs is a special girl.");
 
-
-
+        System.out.println("what is your name?");
+        Scanner lakesideScanner = new Scanner(System.in);
+        String customerName = lakesideScanner.nextLine();
+        System.out.println("Hey, "+ customerName + " welcome to Lakeside today.");
+        System.out.println("What can we do for you?");
 
     }
 }
